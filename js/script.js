8223c55e68613023bf44d5d6463d31c9d89bc3f6
Steven Abaco <stevenabaco@gmail.com>
2021-04-09 10:11:22 -0500

    Complete ShowPage Function

--- js/script.js
+++ js/script.js
@@ -1,30 +1,50 @@
 /*
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
+by Steven Abaco
 */
 
+/**
+ *This function will create and insert/append the elements needed to display a "page" of nine students
+ *@param {array} list Student Data that will be passed as an arguement when the function is called
+ *@param {number} page Page number that will be passed as an arguement when the function is called
+ */
 
+function showPage(list, page) {
+	const itemsPerPage = 9;
+	let startIndex = page * itemsPerPage - itemsPerPage;
+	let endIndex = page * itemsPerPage;
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+	const studentList = document.querySelector(".student-list");
+	studentList.innerHTML = "";
 
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-
-
+	for (let i = 0; i < list.length; i++) {
+		if (i >= startIndex && i < endIndex) {
+         let html = `
+         
+         <li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture">
+               <h3>
+               ${list[i].name.title}
+               ${list[i].name.first}
+               ${list[i].name.last}
+               </h3>
+               <span class="email">${list[i].email}</span>
+    </div>
+    <div class="joined-details">
+      <span class="date">Joined : ${list[i].registered.date}</span>
+    </div>
+  </li>`;
+			studentList.insertAdjacentHTML("beforeend", html);
+		}
+	}
+}
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
-
-
 // Call functions
+showPage(data, 1);
